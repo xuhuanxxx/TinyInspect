a8b4f772b362c2b10dc4863189556cfcd0d8c118
xuhuan <xuhuanxxx@gmail.com>
2026-01-25 03:54:17 +0800

    refactor: item frame alpha setting to always apply

--- InspectUnit.lua
+++ InspectUnit.lua
@@ -286,11 +286,11 @@ function ShowInspectItemListFrame(unit, parent, ilevel, maxLevel)
         if (level > 0) then
             itemframe.levelString:SetText(format(formats,level))
             itemframe.itemString:SetText(link or name)
-            itemframe:SetAlpha(1)
         else
             itemframe.levelString:SetText(format(formats,""))
             itemframe.itemString:SetText("")
         end
+        itemframe:SetAlpha(1)
         if (link and IsCorruptedItem(link)) then
             itemframe.levelString:SetTextColor(0.5, 0.5, 1)
         else
